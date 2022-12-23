aa7ec445516af53cfd08160b551340d50b816c17
Vladyslav Protsenko <vladyslav.protsenko@ucu.edu.ua>
2022-12-23 18:01:24 +0200

    Added zip archive support, symlinks, hardlinks, fixed ls

--- commands.cpp
+++ commands.cpp
@@ -6,7 +6,12 @@
 #include <sys/stat.h>
 #include <pwd.h>
 #include <boost/algorithm/string.hpp>
+#include <time.h>
+#include <zip.h>
+#include <errno.h>
 
+constexpr size_t DATE_LENGTH = 32;
+constexpr size_t STAT_SIZE = 256;
 int vi_cd(const std::vector<std::string> &args) {
     if (args.size() > 1) {
         std::cerr << "cd: too many arguments" << std::endl;
@@ -19,7 +24,7 @@ int vi_cd(const std::vector<std::string> &args) {
     }
     catch(const  std::filesystem::filesystem_error& ex)
     {
-        std::cerr << "cr: " << ex.what() << std::endl;
+        std::cerr << "cd: " << ex.what() << std::endl;
         return DIRECTORY_ERROR;
     }
     catch(const  std::bad_alloc& ex)
@@ -135,9 +140,9 @@ int vi_cat(const std::vector<std::string> &args, std::vector<std::string>* outpu
     return 0;
 }
 
-int vi_ls(const std::vector<std::string> &args, std::string &output){
+int vi_ls(const std::vector<std::string> &args, std::string &output, std::vector<file_data>* metadata){
     std::filesystem::path cur_path = std::filesystem::current_path();
-    std::vector<file_data> metadata;
+//    std::vector<file_data> metadata;
     size_t max_user_len = 0;
     size_t max_size_len = 0;
     size_t max_name_len = 0;
@@ -184,8 +189,7 @@ int vi_ls(const std::vector<std::string> &args, std::string &output){
         }
         else if (((permissions & std::filesystem::perms::owner_exec) != std::filesystem::perms::none ||
                 (permissions & std::filesystem::perms::group_exec) != std::filesystem::perms::none ||
-                (permissions & std::filesystem::perms::others_exec) != std::filesystem::perms::none) &&
-                (!std::filesystem::is_regular_file(status))){
+                (permissions & std::filesystem::perms::others_exec) != std::filesystem::perms::none)){
             file_name = "*" + file_name;
         }
         else if (!std::filesystem::is_regular_file(status)){
@@ -222,45 +226,144 @@ int vi_ls(const std::vector<std::string> &args, std::string &output){
 
         auto mod_time = sb.st_mtime;
         std::tm * ptm = std::localtime(&mod_time);
-        char time_mod_buf[32];
-        std::strftime(time_mod_buf, 32, "%Y-%m-%d %H:%M:%S", ptm);
+        char time_mod_buf[DATE_LENGTH];
+        std::strftime(time_mod_buf, DATE_LENGTH, "%Y-%m-%d %H:%M:%S", ptm);
 
         file_meta.date = std::string{time_mod_buf};
-        metadata.emplace_back(file_meta);
+        metadata->emplace_back(file_meta);
     }
 
     output.reserve(33 + max_user_len + max_size_len + max_name_len);
-    for (size_t i=0; i < metadata.size(); ++i){
+    for (size_t i=0; i < metadata->size(); ++i){
         for (size_t j=0; j<9; ++j){
-            output += metadata[i].rwx[j];
+            output += metadata->at(i).rwx[j];
         }
         output += ' ';
-        for (size_t j=0; j<max_user_len - metadata[i].user.length(); ++j){
+        for (size_t j=0; j<max_user_len - metadata->at(i).user.length(); ++j){
             output += ' ';
         }
-        for (size_t j=0; j<metadata[i].user.length(); ++j){
-            output += metadata[i].user[j];
+        for (size_t j=0; j<metadata->at(i).user.length(); ++j){
+            output += metadata->at(i).user[j];
         }
         output += ' ';
-        for (size_t j=0; j<max_size_len - metadata[i].size.length(); ++j){
+        for (size_t j=0; j<max_size_len - metadata->at(i).size.length(); ++j){
             output += ' ';
         }
-        for (size_t j=0; j<metadata[i].size.length(); ++j){
-            output += metadata[i].size[j];
+        for (size_t j=0; j<metadata->at(i).size.length(); ++j){
+            output += metadata->at(i).size[j];
         }
         output += ' ';
         output += ' ';
-        for (size_t j=0; j<metadata[i].date.length(); ++j){
-            output += metadata[i].date[j];
+        for (size_t j=0; j<metadata->at(i).date.length(); ++j){
+            output += metadata->at(i).date[j];
         }
         output += ' ';
         output += ' ';
-        for (size_t j=0; j<metadata[i].name.length(); ++j){
-            output += metadata[i].name[j];
+        for (size_t j=0; j<metadata->at(i).name.length(); ++j){
+            output += metadata->at(i).name[j];
         }
-        if (i != metadata.size()-1){
+        if (i != metadata->size()-1){
             output += '\n';
         }
     }
     return 0;
+}
+
+// Inspired by https://www.geeksforgeeks.org/c-program-to-read-and-print-all-files-from-a-zip-file/
+int vi_archive(const std::vector<std::string> &args, const std::filesystem::path& path, std::vector<file_archive>* out){
+    zip_t* archive = nullptr;
+    archive = zip_open(path.c_str(), 0, nullptr);
+    struct zip_stat* data = nullptr;
+    data = static_cast<struct zip_stat*>(calloc(STAT_SIZE, sizeof(int)));
+    zip_stat_init(data);
+    zip_file_t* fd = nullptr;
+    char* file = nullptr;
+    int count = 0;
+
+    while ((zip_stat_index(archive, count, 0, data)) == 0) {
+        file = static_cast<char *>(calloc(data->size + 1, sizeof(char)));
+        fd = zip_fopen_index(archive, count, 0);
+        zip_fread(fd, file, data->size);
+        file_archive file_arch;
+        file_arch.name = data->name;
+        file_arch.comp_size = std::to_string(data->comp_size);
+        std::tm * ptm = std::localtime(&data->mtime);
+        char time_mod_buf[DATE_LENGTH];
+        std::strftime(time_mod_buf, DATE_LENGTH, "%Y-%m-%d %H:%M:%S", ptm);
+        file_arch.date = std::string{time_mod_buf};
+        file_arch.content = file;
+
+        out->emplace_back(file_arch);
+
+        free(file);
+        count++;
+    }
+    return 0;
+}
+
+int vi_symlink(const std::vector<std::string> &args){
+    if (args.size() != 2) {
+        std::cerr << "symlink: too few or too many arguments" << std::endl;
+        return ARGUMENTS_ERROR;
+    }
+    try{
+        std::filesystem::create_symlink(std::filesystem::u8path(args[0]), std::filesystem::u8path(args[1]));
+    }
+    catch(const  std::filesystem::filesystem_error& ex)
+    {
+        std::cerr << "symlink: link filename exists" << std::endl << ex.what() << std::endl;
+        return ARGUMENTS_ERROR;
+    }
+    return 0;
+}
+
+int vi_hardlink(const std::vector<std::string> &args){
+    if (args.size() != 2) {
+        std::cerr << "hardlink: too few or too many arguments" << std::endl;
+        return ARGUMENTS_ERROR;
+    }
+    try{
+        std::filesystem::create_hard_link(std::filesystem::u8path(args[0]), std::filesystem::u8path(args[1]));
+    }
+    catch(const  std::filesystem::filesystem_error& ex)
+    {
+        std::cerr << "hardlink: link filename exists" << std::endl << ex.what() << std::endl;
+        return ARGUMENTS_ERROR;
+    }
+    return 0;
+}
+
+int vi_chmod(const std::vector<std::string> &args){
+    std::filesystem::perms params = std::filesystem::perms::none;
+    if (args[1] == "r"){
+        params |= std::filesystem::perms::owner_read;
+    }
+    if (args[2] == "w"){
+        params |= std::filesystem::perms::owner_write;
+    }
+    if (args[3] == "x"){
+        params |= std::filesystem::perms::owner_exec;
+    }
+    if (args[4] == "r"){
+        params |= std::filesystem::perms::group_read;
+    }
+    if (args[5] == "w"){
+        params |= std::filesystem::perms::group_write;
+    }
+    if (args[6] == "x"){
+        params |= std::filesystem::perms::group_exec;
+    }
+    if (args[7] == "r"){
+        params |= std::filesystem::perms::others_read;
+    }
+    if (args[8] == "w"){
+        params |= std::filesystem::perms::others_write;
+    }
+    if (args[9] == "x"){
+        params |= std::filesystem::perms::others_exec;
+    }
+
+    std::filesystem::permissions(std::filesystem::u8path(args[0]),
+                    params,
+                    std::filesystem::perm_options::replace);
 }
--- main.cpp
+++ main.cpp
@@ -2,43 +2,91 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include "commands.h"
-
+#include <chrono>
+#include <thread>
 int main(int argc, char *argv[]) {
 //    command_line_options_t command_line_options{argc, argv};
 //    std::cout << "A flag value: " << command_line_options.get_A_flag() << std::endl;
 
 // Testing cd:
-    std::vector<std::string> input = {".."};
+//    std::vector<std::string> input = {".."};
 //    std::cout << std::filesystem::current_path() << std::endl;
-    vi_cd(input);
+//    vi_cd(input);
 //    std::cout << std::filesystem::current_path() << std::endl;
 
 // Testing mv:
-    std::vector<std::string> input2 = {"hello.txt", "helloe.txt"};
-    std::vector<std::string> input3 = {"prikol", "data"};
+//    std::vector<std::string> input2 = {"hello.txt", "helloe.txt"};
+//    std::vector<std::string> input3 = {"prikol", "data"};
 //    vi_mv(input2);
 //    vi_mv(input3);
 
 // Testing mkdir:
-    std::vector<std::string> input4 = {"new dir", "yomayo", "gg"};
+//    std::vector<std::string> input4 = {"new dir", "yomayo", "gg"};
 //    vi_mkdir(input4);
 
 // Testing touch:
-    std::vector<std::string> input5 = {"touch.txt", "yoma.yaml", "gg.py"};
+//    std::vector<std::string> input5 = {"touch.txt", "yoma.yaml", "gg.py"};
 //    vi_touch(input5);
 
 //  Testing cat:
-    std::vector<std::string> output;
-    std::vector<std::string> input6 = {"hello2.txt", "helloe.txt"};
-    vi_cat(input6, &output);
+//    std::vector<std::string> output;
+//    std::vector<std::string> input6 = {"hello2.txt", "helloe.txt"};
+//    vi_cat(input6, &output);
 //    for (auto &el: output){
 //        std::cout << el << std::endl;
 //    }
 
 //  Testing ls:
-    std::vector<std::string> input7 = {};
-    std::string ls{};
-    vi_ls(input7, ls);
-    std::cout << ls << std::endl;
+//    std::vector<std::string> input7 = {};
+//    std::string ls{};
+//    std::vector<file_data> info;
+//    std::vector<file_data>* info_ptr = &info;
+//    vi_ls(input7, ls, info_ptr);
+//    std::cout << ls << std::endl;
+//    for (auto el:info){
+//        std::cout << el.name << "   " << el.rwx << "    " << el.user << "    " << el.size << "    " << el.date << std::endl;
+//    }
+//  Testing archive:
+//    std::vector<file_archive> inside_zip;
+//    std::vector<file_archive>* inside_zip_ptr = &inside_zip;
+//    vi_archive(input7, "../data/data.zip", inside_zip_ptr);
+//    for (auto el:inside_zip){
+//        std::cout << el.name << "   " << el.comp_size << "    " << el.date << "    " << el.content << std::endl;
+//    }
+//  Testing symlink:
+//    std::vector<std::string> args = {"../data/joj.txt", "sym_joj.txt"};
+//    vi_symlink(args);
+//    std::vector<std::string> input7 = {};
+//    std::string ls{};
+//    std::vector<file_data> info;
+//    std::vector<file_data>* info_ptr = &info;
+//    vi_ls(input7, ls, info_ptr);
+//    std::cout << ls << std::endl;
+
+//  Testing hardlink:
+//    std::vector<std::string> args = {"../data/hard_joj.txt", "hard_joj.txt"};
+//    vi_hardlink(args);
+//    std::vector<std::string> input7 = {};
+//    std::string ls{};
+//    std::vector<file_data> info;
+//    std::vector<file_data>* info_ptr = &info;
+//    vi_ls(input7, ls, info_ptr);
+//    std::cout << ls << std::endl;
+
+// Testing chmod:
+//    std::vector<std::string> input7 = {};
+//    std::string ls{};
+//    std::vector<file_data> info;
+//    std::vector<file_data>* info_ptr = &info;
+//    vi_ls(input7, ls, info_ptr);
+//    std::cout << ls << std::endl;
+//    std::vector<std::string> inputn = {"opapa.txt", "r", "w", "x", "", "w", "x", "-", "-", "-"};
+//    vi_chmod(inputn);
+//    ls = {};
+//    info = {};
+//    info_ptr = &info;
+//    vi_ls(input7, ls, info_ptr);
+//    std::cout << std::endl;
+//    std::cout << ls << std::endl;
     return 0;
 }
